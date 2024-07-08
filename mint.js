76bf0e6258ba978421981a2d5f53e69d9eb14056
Irisong0818 <iris08186287@gmail.com>
2024-07-08 22:48:45 +0900

    수정수정

--- mint.js
+++ mint.js
@@ -1,6 +1,22 @@
 $(function(){
     $(".type-list").click(function(){
-    $(".selected").removeClass("selected")
-    $(this).addClass("selected")
+        $(".selected").removeClass("selected")
+        $(this).addClass("selected")
+    })    
+
+    $("#category-items>p").click(function(){
+        $(".category-selected").removeClass("category-selected")
+        $(this).addClass("category-selected")
+    })
+
 })
-})
+
+
+
+//html
+
+
+//function add(a, b){
+//    return a + b
+//}
+//const add = (a,b) => a + b
